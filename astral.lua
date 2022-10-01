905ae708d0eb57c316cffa0783fe96cccfb7ac6a
NTDCore <102583640+NTDCore@users.noreply.github.com>
2022-10-01 08:43:11 +0700

    Update astral.lua

--- astral.lua
+++ astral.lua
@@ -16,7 +16,7 @@ local Notifications = loadstring(game:HttpGet("https://raw.githubusercontent.com
 getgenv().notifs = Notifications
 local SwordAnimations = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mastadawn/AstralV2/main/SwordAnimations.lua", true))()
 local Chat = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mastadawn/AstralV2/main/chatthingy.lua", true))()
-local ArrayList = loadstring(game:HttpGet("https://raw.githubusercontent.com/Mastadawn/AstralV2/main/Arraylist.lua", true))()
+local ArrayList = loadstring(game:HttpGet("https://raw.githubusercontent.com/NTDCore/AstralV2/main/Arraylist.lua", true))()
 
 if not isfolder("Astral") then
 	makefolder("Astral")
@@ -26,7 +26,9 @@ if not isfile("Astral/Scripts.lua") then
 	writefile("Astral/Scripts.lua",scriptExample)
 end
 
+local players = game:GetService("Players")
 local player = game.Players.LocalPlayer
+local lplr = players.LocalPlayer
 
 local Utilities
 local Bedwars
@@ -637,7 +639,95 @@ UILibrary.MakeModule{
 	Description = "Automatically stores items in enderchest until you are near a shop",
 	AntivoidPart = nil
 }
-
+local function Cape(char, texture)
+	for i,v in pairs(char:GetDescendants()) do
+		if v.Name == "Cape" then
+			v:Remove()
+		end
+	end
+	local hum = char:WaitForChild("Humanoid")
+	if char:FindFirstChild("Torso") then
+		torso = char.Torso
+	else
+		torso = char.UpperTorso
+	end
+	local p = Instance.new("Part", torso.Parent)
+	p.Name = "Cape"
+	p.Anchored = false
+	p.CanCollide = false
+	p.TopSurface = 0
+	p.BottomSurface = 0
+	p.FormFactor = "Custom"
+	p.Size = Vector3.new(0.2,0.2,0.2)
+	p.Transparency = 1
+	local decal = Instance.new("Decal", p)
+	decal.Texture = texture
+	decal.Face = "Back"
+	local msh = Instance.new("BlockMesh", p)
+	msh.Scale = Vector3.new(9,17.5,0.5)
+	local motor = Instance.new("Motor", p)
+	motor.Part0 = p
+	motor.Part1 = torso
+	motor.MaxVelocity = 0.01
+	motor.C0 = CFrame.new(0,2,0) * CFrame.Angles(0,math.rad(90),0)
+	motor.C1 = CFrame.new(0,1,0.45) * CFrame.Angles(0,math.rad(90),0)
+	local wave = false
+	repeat wait(1/44)
+		decal.Transparency = torso.Transparency
+		local ang = 0.1
+		local oldmag = torso.Velocity.magnitude
+		local mv = 0.002
+		if wave then
+			ang = ang + ((torso.Velocity.magnitude/10) * 0.05) + 0.05
+			wave = false
+		else
+			wave = true
+		end
+		ang = ang + math.min(torso.Velocity.magnitude/11, 0.5)
+		motor.MaxVelocity = math.min((torso.Velocity.magnitude/111), 0.04) --+ mv
+		motor.DesiredAngle = -ang
+		if motor.CurrentAngle < -0.2 and motor.DesiredAngle > -0.2 then
+			motor.MaxVelocity = 0.04
+		end
+		repeat wait() until motor.CurrentAngle == motor.DesiredAngle or math.abs(torso.Velocity.magnitude - oldmag) >= (torso.Velocity.magnitude/10) + 1
+		if torso.Velocity.magnitude < 0.1 then
+			wait(0.1)
+		end
+	until not p or p.Parent ~= torso.Parent
+end
+--[[
+UILibrary.MakeModule{
+	Name = "Cape",
+	Tab = Visual,
+	Bind = nil,
+	Enabled = false,
+	Function = function(Module, Dropdowns)
+		lplr.CharacterAdded:Connect(function(char)
+			spawn(function()
+				pcall(function() 
+					Cape(char, ("http://www.roblox.com/asset/?id=11121170269"))
+				end)
+			end)
+		end)
+		if lplr.Character then
+			spawn(function()
+				pcall(function() 
+					Cape(lplr.Character, ("http://www.roblox.com/asset/?id=11121170269"))
+				end)
+			end)
+		end
+		Module.Enabled == false
+		if lplr.Character then
+			for i,v in pairs(lplr.Character:GetDescendants()) do
+				if v.Name == "Cape" then
+					v:Remove()
+				end
+			end
+		end
+	end,
+	Description = "idk lol",
+}
+--]]
 UILibrary.MakeModule{
 	Name = "Lighting",
 	Tab = Visual,
